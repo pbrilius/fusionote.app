c194a75270aa71e4c5297854f73ca78bf62cc6cd
Povilas Brilius <pbrilius@gmail.com>
2020-03-02 13:47:53 +0200

    Immutable hash and jsonfied properties.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -2,7 +2,32 @@
 
 use CodeIgniter\Entity;
 
-class Note extends Entity
+class User extends Entity
 {
-    
+    protected $attributes = [
+        'id' => null,
+        'email' => null,
+        'hash' => null,
+        'roles' => null,
+        'attributes' => null,
+    ];
+
+    protected $casts = [
+        'roles' => 'json',
+        'attributes' => 'json',
+    ];
+
+    /**
+     * Immutable hash
+     *
+     * @param string $hash
+     * @return void
+     */
+    public function setHash(string $hash)
+    {
+        $object = new User(get_object_vars($this));
+        $object->hash = $hash;
+
+        return $object;
+    }
 }
